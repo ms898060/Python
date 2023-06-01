3cab3075583eb60baccac169f3891a1d1d717189
Matheus Araújo <66802785+ms898060@users.noreply.github.com>
2023-06-01 12:23:52 -0300

    Strings

---
+++ String.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Strings: Valores de Texto Compostos por uma sequencia de caracteres
+
+# In[1]:
+
+
+matheus
+
+
+# In[2]:
+
+
+'matheus'
+
+
+# In[3]:
+
+
+"matheus"
+
+
+# In[4]:
+
+
+print('matheus')
+
+
+# In[5]:
+
+
+print("matheus")
+
+
+# In[7]:
+
+
+y = 7
+print( y + 'Reais')
+
+
+# In[9]:
+
+
+y = 7
+print(str(y) + ' Reais')
+
+
+# In[10]:
+
+
+"I'm fine"
+
+
+# In[12]:
+
+
+'I\'m fine'
+
+
+# In[13]:
+
+
+'Aperte Aqui'
+
+
+# In[16]:
+
+
+'Aperte' ' Aqui'
+
+
+# In[ ]:
+
+
+
+
